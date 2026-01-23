1fd99d08b4fd819e7a7b7e09a81c4245be95ac1d
silvadavilucas630-del <silvadavilucas630@gmail.com>
2026-01-23 12:50:39 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,38 @@
+const produtos = [
+  {
+    marca: "xiaomi",
+    nome: "Xiaomi Redmi Note 12",
+    imagem: "produtos/redmi12.png",
+    memorias: {
+      "128GB": 1200,
+      "256GB": 1450
+    }
+  },
+  {
+    marca: "iphone",
+    nome: "iPhone 11",
+    imagem: "produtos/iphone11.png",
+    memorias: {
+      "64GB": 2300,
+      "128GB": 2700
+    }
+  },
+  {
+    marca: "poco",
+    nome: "POCO X5",
+    imagem: "produtos/pocox5.png",
+    memorias: {
+      "128GB": 1500,
+      "256GB": 1800
+    }
+  },
+  {
+    marca: "realme",
+    nome: "Realme C75",
+    imagem: "produtos/realme.png",
+    memorias: {
+      "128GB": 1100,
+      "256GB": 1350
+    }
+  }
+];
