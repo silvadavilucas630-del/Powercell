56fe4328b3a8677c7592d50cca1505f42b26203a
silvadavilucas630-del <silvadavilucas630@gmail.com>
2026-01-23 16:33:11 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,38 +1,42 @@
 const produtos = [
   {
-    marca: "xiaomi",
-    nome: "Xiaomi Redmi Note 12",
-    imagem: "produtos/redmi12.png",
-    memorias: {
-      "128GB": 1200,
-      "256GB": 1450
-    }
-  },
-  {
-    marca: "iphone",
+    id: 1,
+    marca: "apple",
     nome: "iPhone 11",
-    imagem: "produtos/iphone11.png",
+    imagem: "img/iphone11.png",
     memorias: {
       "64GB": 2300,
-      "128GB": 2700
+      "128GB": 2600
     }
   },
   {
+    id: 2,
+    marca: "xiaomi",
+    nome: "Redmi Note 12",
+    imagem: "img/redmi12.png",
+    memorias: {
+      "128GB": 1200,
+      "256GB": 1400
+    }
+  },
+  {
+    id: 3,
     marca: "poco",
     nome: "POCO X5",
-    imagem: "produtos/pocox5.png",
+    imagem: "img/pocox5.png",
     memorias: {
       "128GB": 1500,
-      "256GB": 1800
+      "256GB": 1700
     }
   },
   {
+    id: 4,
     marca: "realme",
     nome: "Realme C75",
-    imagem: "produtos/realme.png",
+    imagem: "img/realmec75.png",
     memorias: {
       "128GB": 1100,
-      "256GB": 1350
+      "256GB": 1300
     }
   }
 ];
